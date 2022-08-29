4b053170edb12b98c7e9fc4aa663bd40729fe94c
Sarah Wiedrich <97638277+SarahWiedrich@users.noreply.github.com>
2022-08-29 11:32:43 -0500

    functions complete and work

---
+++ functions.py
@@ -0,0 +1,38 @@
+#return max number
+def max_num(*num):
+    x = max(*num)
+    print(x)
+
+max_num(3, 6, 7, 12, 22)
+
+#multiply all the numbers in a list
+def mult_list(my_list):
+    result = 1
+    for num in my_list:
+        result = result * num
+    return result
+
+list1 = [2, 4, 6, 8]
+list2 = [1, 3, 5, 7]
+
+print(mult_list(list1))
+print(mult_list(list2))
+    
+
+#reverse a string
+def rev_string(text):
+    reverse = text [::-1]
+    print(reverse)
+
+rev_string("Hello")
+
+#check if a number falls in a given range
+def num_within(num):
+    if num in my_range :
+        print("in range")
+    else:
+        print("not in range")
+
+my_range = range(2, 3, 4)
+num_within(2)
+
